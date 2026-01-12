ab90d6c80accab2ab6102e29c66ebfad26b45bcb
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2026-01-12 10:35:35 +0200

    seperate layouts and fix translation error and direction error

---
+++ src/app/layout.tsx
@@ -0,0 +1,28 @@
+import "./globals.css";
+import { AppProvider } from "@/context/AppContext";
+import { UserProvider } from "@/context/UserContext";
+import AuthProvider from "@/providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <html>
+        <body suppressHydrationWarning={true} className="">
+          <UserProvider>
+            <AppProvider>
+              <AuthProvider>
+                {children}
+                <ToastContainer position="top-right" autoClose={3000} />
+              </AuthProvider>
+            </AppProvider>
+          </UserProvider>
+        </body>
+      </html>
+    </>
+  );
+}
