2e371a729dee830f400a3407c2bf0fe9211cf9d2
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2026-01-18 11:00:20 +0200

    finish top player section in home page

--- src/app/[locale]/(main)/(home)/_sections/TriviaLeaderboards/index.tsx
+++ src/app/[locale]/(main)/(home)/_sections/TriviaLeaderboards/index.tsx
@@ -1,11 +1,45 @@
-import { AllLeaderboards } from "@/utils/types/Home/AllLeaderboards"
+import { AllLeaderboards } from "@/utils/types/Home/AllLeaderboards";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 
-export default function HomeTriviaLeaderboardSection({
-  data
+export default function HomeAllLeaderboardSection({
+  data,
 }: {
-  data: AllLeaderboards[]
+  data: AllLeaderboards[];
 }) {
+  const t = useTranslations("pages.main.home.all_leaderboard");
   return (
-    <div>HomeTriviaLeaderboardSection</div>
-  )
+    <div className="py-20 mt-10 px-4 lg:px-8 flex flex-col items-center justify-center gap-8 bg-[url('/images/home/top-player-bg.png')] bg-cover bg-center text-white ">
+      <h2 className="!font-bold lg:!text-4xl md:mb-2 xl:mb-6">{t("title")}</h2>
+      <div className="container space-y-6">
+        {/* first */}
+        <div className="border-gradient-primary bg-black py-4 xl:py-6 px-6 xl:px-10 max-w-5xl mx-auto rounded-xl lg:rounded-2xl flex items-center justify-between gap-4 lg:gap-6 hover:shadow-xl hover:shadow-[#FFCA11] hover:scale-105 transition-all duration-300">
+          <Image src="/images/home/top-player-1.png" alt="First Player" width={1000} height={1000} className="w-8 lg:w-14"/>
+            <p className="text-xl font-semibold flex-grow lg:text-3xl">{data[0].username}</p>
+            <div className={`flex items-center gap-2 ${data[0].score_hide ? "blur-sm" : ""}`}>
+              <p>{data[0].points}</p>
+              <span className="text-sm">{t("points")}</span>
+            </div>
+        </div>
+        {/* Second */}
+        <div className="border-gradient-primary bg-black py-4 xl:py-6 px-6 xl:px-10 max-w-5xl mx-auto rounded-xl lg:rounded-2xl flex items-center justify-between gap-4 lg:gap-6 hover:shadow-xl hover:shadow-[#AEEEFA] hover:scale-105 transition-all duration-300">
+          <Image src="/images/home/top-player-2.png" alt="First Player" width={1000} height={1000} className="w-8 lg:w-14"/>
+            <p className="text-xl font-semibold flex-grow lg:text-3xl">{data[1].username}</p>
+            <div className={`flex items-center gap-2 ${data[1].score_hide ? "blur-sm" : ""}`}>
+              <p>{data[1].points}</p>
+              <span className="text-sm">{t("points")}</span>
+            </div>
+        </div>
+        {/* Third */}
+        <div className="border-gradient-primary bg-black py-4 xl:py-6 px-6 xl:px-10 max-w-5xl mx-auto rounded-xl lg:rounded-2xl flex items-center justify-between gap-4 lg:gap-6 hover:shadow-xl hover:shadow-[#DE5C21] hover:scale-105 transition-all duration-300">
+          <Image src="/images/home/top-player-3.png" alt="First Player" width={1000} height={1000} className="w-8 lg:w-14"/>
+            <p className="text-xl font-semibold flex-grow lg:text-3xl">{data[2].username}</p>
+            <div className={`flex items-center gap-2 ${data[2].score_hide ? "blur-sm" : ""}`}>
+              <p>{data[2].points}</p>
+              <span className="text-sm">{t("points")}</span>
+            </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/[locale]/(main)/(home)/page.tsx
+++ src/app/[locale]/(main)/(home)/page.tsx
@@ -2,7 +2,7 @@ import { homeFetch } from "@/lib/api/home/HomeFetch";
 import HomeHeroSection from "./_sections/HeroSection";
 import HomeNewsSection from "./_sections/News";
 import HomeUserStatsSection from "./_sections/UserState";
-import HomeTriviaLeaderboardSection from "./_sections/TriviaLeaderboards";
+import HomeAllLeaderboardSection from "./_sections/TriviaLeaderboards";
 import TowButtonsSection from "./_sections/TowButtons";
 
 export default async function HomeContentPage() {
@@ -13,7 +13,7 @@ export default async function HomeContentPage() {
       {data?.news && <HomeNewsSection data={data.news} />}
       {data?.user && <HomeUserStatsSection data={data.user} />}
       <TowButtonsSection/>
-      {data?.all_leaderboard && <HomeTriviaLeaderboardSection data={data.all_leaderboard} />}
+      {data?.all_leaderboard && <HomeAllLeaderboardSection data={data.all_leaderboard} />}
     </div>
   );
 }
