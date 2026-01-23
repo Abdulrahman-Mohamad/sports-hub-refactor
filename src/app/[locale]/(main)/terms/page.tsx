39079b9cdc2596b9f39cba30bb49b20659026968
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2026-01-24 00:48:25 +0200

    gettig start with fixture page

--- src/app/[locale]/(main)/terms/page.tsx
+++ src/app/[locale]/(main)/terms/page.tsx
@@ -2,13 +2,9 @@ import { termsFetch } from "@/lib/api/terms/TermsFetch";
 import TermsHeroSection from "./_section/Hero";
 import TermsSection from "./_section/Terms";
 
-type termsData = {
-  content: string;
-};
-
 export default async function TermsPage() {
   const res = await termsFetch();
-  const data: termsData = res?.data;
+  const data = res?.data;
   return (
     <>
       <TermsHeroSection />
