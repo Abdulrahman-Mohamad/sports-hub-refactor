63f419d5d35e036baebedfaa3d690f1e0afd12aa
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2026-02-09 02:20:56 +0200

    finish redesign and translate GamesInsufficientModal successfully

--- src/components/ui/SingleGame/Modals/InsufficientModal/index.tsx
+++ src/components/ui/SingleGame/Modals/InsufficientModal/index.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
 import React from "react";
-// import sadGIF from "@/assets/games/sad.gif";
 import { usePathname, useRouter } from "next/navigation";
 import { useLocale, useTranslations } from "next-intl";
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 export default function GamesInsufficientModal({
   onClose,
@@ -12,7 +11,7 @@ export default function GamesInsufficientModal({
   onClose: () => void;
   setStep: (step: "check" | "insufficient") => any;
 }) {
-  const t = useTranslations();
+  const t = useTranslations('components.ui.InsufficientModal');
   const router = useRouter();
   const locale = useLocale();
   const pathname = usePathname();
@@ -24,40 +23,40 @@ export default function GamesInsufficientModal({
   };
   return (
     <>
-      <div className="border-gradient-greenblue-empty glow-blue-green-notification">
+      <div className="bg-white rounded-xl">
         <div className="w-full px-8 md:px-20 py-6">
-          <h3 className="text-gradient-bluegreenA1 font-medium text-center text-2xl  md:text-3xl">
-            {t("components.games_card.insufficient_coins")}
+          <h3 className="text-gradient-primary !font-semibold text-center !text-2xl md:!text-3xl">
+            {t("title")}
           </h3>
         </div>
         <div className="px-12 py-8 flex flex-col gap-4 items-center max-w-2xl mx-auto ">
           <div>
-            {/* <Image
-              src={sadGIF}
+            <Image
+              src={'/images/common/sad-face.png'}
               alt="Coin"
               width={200}
               height={200}
               className="aspect-square w-[100px] md:w-[150px]"
               unoptimized
-            /> */}
+            />
           </div>
-          <p className="text-center text-sm md:text-base text-white w-3/4 mx-auto my-10">
-            {t("components.games_card.insufficient_message")}
+          <p className="text-center text-sm md:text-base lg:text-lg font-semibold w-3/4 mx-auto my-10">
+            {t("description")}
           </p>
           <div className="grid grid-cols-2 gap-4 w-full md:w-3/4">
             <button
-              className="btn bg-white border-2 border-blueA1 text-blueA1 hover:bg-white/80 "
+              className="btn bg-white border-2 border-gradient-primary text-gradient-primary !rounded-full hover:bg-white/80 "
               onClick={handleClose}
             >
-              {t("common.no_thanks")}
+              {t("no_thanks")}
             </button>
             <motion.button
               whileTap={{ scale: 0.9 }}
               whileHover={{ scale: 1.1 }}
-              className="btn bg-gradient-bluegreenA1 text-white !font-bold"
+              className="btn bg-gradient-primary text-white !font-bold !rounded-full"
               onClick={() => router.push("/packages")}
             >
-              {t("components.games_card.go_to_packages")}
+              {t("yes")}
             </motion.button>
           </div>
         </div>
