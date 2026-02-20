3cb924c7b4235110ed11218067a8c11746ad4cc2
Abdulrahman-Mohamad <abd.rahman.mohamad.93@gmail.com>
2026-02-20 02:27:45 +0200

    finish refactor hero section in all main pages

--- src/app/[locale]/(main)/history/_sections/Hero/index.tsx
+++ src/app/[locale]/(main)/history/_sections/Hero/index.tsx
@@ -1,34 +1,19 @@
+// history-hero-bg-mobile.png
+
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 
 export default function HistoryHeroSection() {
   const t = useTranslations();
   return (
     <>
       <div className="pt-5 px-4 sm:px-6 sm:pt-7 lg:px-8 lg:pt-4 z-10 relative">
-        <div className="relative w-full min-h-[550px] rounded-2xl flex-center flex-col lg:aspect-[1303/861] overflow-hidden">
-          <Image
-            src="/images/history/history-hero-bg.png"
-            alt={t("pages.main.history.hero.title")}
-            fill
-            priority
-            className="object-cover hidden lg:block"
-          />
-          <Image
-            src="/images/history/history-hero-bg-mobile.png"
-            alt={t("pages.main.history.hero.title")}
-            fill
-            priority
-            className="object-cover lg:hidden"
-          />
-          <div className="relative z-10 flex flex-col items-center justify-center xl:pb-30 2xl:pb-40">
-            <h1 className="text-white !text-[60px] !font-extrabold hero-shadow-bold md:!text-[80px] 2xl:!text-[120px]">
-              {t("pages.main.history.hero.title")}
-            </h1>
-            <p className="text-white text-center font-bold px-6 mt-6 text-lg hero-shadow-light md:w-3/4 md:text-2xl 2xl:text-3xl">
-              {t("pages.main.history.hero.description")}
-            </p>
-          </div>
+        <div className="bg-[url('/images/history/history-hero-bg-mobile.png')] bg-cover bg-center bg-no-repeat w-full min-h-[550px] rounded-2xl flex-center flex-col lg:bg-[url('/images/history/history-hero-bg.png')] lg:aspect-[1303/861] xl:pb-30 2xl:pb-40">
+          <h1 className="text-white !text-[60px] !font-extrabold hero-shadow-bold md:!text-[80px] 2xl:!text-[120px]">
+            {t("pages.main.history.hero.title")}
+          </h1>
+          <p className="text-white text-center font-bold px-6 mt-6 text-lg hero-shadow-light md:w-3/4 md:text-2xl 2xl:text-3xl">
+            {t("pages.main.history.hero.description")}
+          </p>
         </div>
       </div>
     </>
